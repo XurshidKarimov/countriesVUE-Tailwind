466d4c66ed0dbb2caa59bc8fdad0016023b6ce77
Xurshid <kaxudi34801@gmail.com>
2023-02-26 00:13:13 +0500

    initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import { createStore } from 'vuex'
+import { getData } from "./getData/main"
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+const store = createStore({
   modules: {
-  }
+    getDataMoudle: {
+      getData
+    }
+  },
 })
+
+export default store;
